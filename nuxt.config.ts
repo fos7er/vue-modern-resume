af5985a1e144d28daa1424ffe039f6dfe197181e
Alexandr Kurkin <2012stalker@gmail.com>
2023-10-10 14:44:12 +0300

    changed google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,8 +13,11 @@ export default defineNuxtConfig({
       link: [
         {rel: 'icon', type: 'image/png', href: '/favicon.png'},
         {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
-        {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
-        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500&display=swap' },
+        {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous'},
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500&display=swap'
+        },
       ],
       meta: [
         {name: 'robots', content: 'index, follow'},
