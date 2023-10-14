435eed303c399017e82121d0b2df0274f92d994f
Alexandr Kurkin <2012stalker@gmail.com>
2023-10-14 18:02:46 +0300

    added gallery

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500&display=swap'
-        },
+        }
       ],
       meta: [
         {name: 'robots', content: 'index, follow'},
--- src/components/block/Card.vue
+++ src/components/block/Card.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="card bg-white">
-    <img class="card__img cursor-zoom-in" src="https://stozen-spb.github.io/images/portfolio/01.jpg" alt="sds">
+    <lightgallery :settings="gallerySettings">
+      <a href="https://stozen-spb.github.io/images/portfolio/01.jpg">
+        <img class="card__img cursor-zoom-in" src="https://stozen-spb.github.io/images/portfolio/01.jpg" alt="sds">
+      </a>
+    </lightgallery>
     <div class="card_title text-center">Smirkspace</div>
   </div>
 </template>
 
 <script setup>
+  import Lightgallery from 'lightgallery/vue'
+  import lgZoom from 'lightgallery/plugins/zoom'
 
+  const plugins = [lgZoom]
+  const gallerySettings = {
+    speed: 500,
+    plugins: plugins,
+    mode: 'lg-zoom',
+    download: false,
+    counter: false,
+    zoom: false
+  }
 </script>
 
 <style lang="scss" scoped>
-
+  @import 'lightgallery/scss/lightgallery.scss';
+  @import 'lightgallery/scss/lg-zoom.scss';
 </style>
+
