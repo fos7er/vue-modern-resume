86885cff1658d61dd2182f099ecbb7a352951997
Alexandr Kurkin <2012stalker@gmail.com>
2023-10-15 17:19:36 +0300

    test

--- src/components/block/Card.vue
+++ src/components/block/Card.vue
@@ -2,7 +2,7 @@
   <div class="card bg-white">
     <lightgallery :settings="gallerySettings">
       <a href="https://stozen-spb.github.io/images/portfolio/01.jpg">
-        <img class="card__img cursor-zoom-in" src="https://stozen-spb.github.io/images/portfolio/01.jpg" alt="sds">
+        <img class="card__img cursor-zoom-in" src="https://stozen-spb.github.io/images/portfolio/01.jpg" alt="ssdfds">
       </a>
     </lightgallery>
     <div class="card_title text-center">Smirkspace</div>
