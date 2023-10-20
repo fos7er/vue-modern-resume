9d13e4ca1f8b482adb41035f8c36af7cead55374
Alexandr Kurkin <2012stalker@gmail.com>
2023-10-20 17:48:06 +0300

    wip skills section

---
+++ src/components/element/TitleBordered.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pb-12 text-md font-bold text-black/70 text-center md:text-left">
+    <h2><slot/></h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  h2 {
+    @apply inline-block pb-1 border-b-[3px] border-blue-600 uppercase
+  }
+</style>
---
+++ src/components/section/Skills.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="py-12">
+    <div class="container">
+      <div class="header md:grid grid-cols-4">
+        <element-title-bordered>{{ t('skills.title') }}</element-title-bordered>
+        <div class="description col-span-3">
+          <p class="mb-8">{{ t('skills.subtitle' ) }}</p>
+        </div>
+      </div>
+      <div class="body">
+        <element-skill value="80">Name</element-skill>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+  const { t } = useI18n()
+</script>
+
+<style lang="scss" scoped>
+
+</style>
