f2ceb04f636712aa9c7dffed6b76c39190dd8fe1
Alexandr Kurkin <2012stalker@gmail.com>
2023-10-22 00:42:43 +0300

    sort

--- src/components/section/Portfolio.vue
+++ src/components/section/Portfolio.vue
@@ -1,10 +1,13 @@
 <template>
   <section class="bg-grey py-[90px]">
     <div class="container">
-      <h2 class="mb-12 text-center uppercase font-medium text-[#95A3A3] tracking-wider">{{ t('worksTitle') }}</h2>
+      <h2 class="mb-12 text-center uppercase font-medium text-[#95A3A3] tracking-wider">
+        {{ t('portfolioTitle') }}
+      </h2>
       <div class="sm:grid grid-cols-3 lg:grid-cols-4 gap-10">
         <block-card
           v-for="item in works"
+          :key="item.id"
           :img-src="getFile(item.img)"
           :nuxt-picture-src="item.img"
           :sub-html="item.subHtml"
@@ -22,61 +25,73 @@
   const getFile = (file) => images[`../../assets/img/portfolio/${file}`]?.default
   const works = [
     {
+      id: 1,
       img: '01.jpg',
       title: 'Canadian Wanderlust',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
     },
     {
+      id: 2,
       img: '02.jpg',
       title: 'Fury Fighting Gear',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
     },
     {
+      id: 3,
       img: '03.jpg',
       title: 'Original Thai Food',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
     },
     {
+      id: 4,
       img: '04.jpg',
       title: 'Resume Website',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
     },
     {
+      id: 5,
       img: '05.jpg',
       title: 'Smirkspace',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
     },
     {
+      id: 6,
       img: '06.jpg',
       title: 'Canadian Wanderlust',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
     },
     {
+      id: 7,
       img: '01.jpg',
       title: 'Fury Fighting Gear',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
     },
     {
+      id: 8,
       img: '02.jpg',
       title: 'Original Thai Food',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
     },
     {
+      id: 9,
       img: '03.jpg',
       title: 'Resume Website',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
     },
     {
+      id: 10,
       img: '04.jpg',
       title: 'Smirkspace',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
     },
     {
+      id: 11,
       img: '05.jpg',
       title: 'Canadian Wanderlust',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
     },
     {
+      id: 12,
       img: '06.jpg',
       title: 'Fury Fighting Gear',
       subHtml: '<h4> Source on - <a href=\'https://github.com\' > GitHub </a></h4> <p> My awesome  work example</p>'
