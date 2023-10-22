df136b77dd9d3c9d285bbc5db049eb20ae8751ef
Alexandr Kurkin <2012stalker@gmail.com>
2023-10-23 00:20:59 +0300

    redesign

--- src/components/block/Bio.vue
+++ src/components/block/Bio.vue
@@ -2,7 +2,7 @@
   <div class="mb-8">
     <h3 class="mb-2 text-lg font-bold text-dark">{{ title }}</h3>
     <p class="mb-4">
-      <span class="italic text-gray-500 font-roboto">{{ subtitle }}</span>
+      <span class="italic text-gray-500 font-roboto text-[18px]">{{ subtitle }}</span>
       <span class="px-2.5">•</span>
       <span>{{ period }}</span>
     </p>
--- src/components/section/About.vue
+++ src/components/section/About.vue
@@ -1,12 +1,12 @@
 <template>
-  <section class="bg-[#000524] text-white">
+  <section class="bg-[#2b2b2b] text-[#7A7A7A]">
     <div class="container about pt-24 pb-16">
       <div class="md:grid grid-cols-4 items-start">
         <div class="photo flex justify-center md:justify-start">
           <img class="mb-6 rounded-full w-[114px]" src="@/assets/img/myPhoto.jpeg" alt="myPhoto">
         </div>
         <div class="col-span-3">
-          <h3 class="mb-3 text-lg font-bold">About Me</h3>
+          <h3 class="mb-3 text-lg text-white font-bold">About Me</h3>
           <p class="mb-6">
             Use this bio section as your way of describing yourself and saying what you do, what technologies you like
             to
@@ -14,14 +14,14 @@
           </p>
           <div class="md:grid grid-cols-2 gap-2">
             <div class="mb-6">
-              <h3 class="mb-3 text-lg font-bold">Contact Details</h3>
+              <h3 class="mb-3 text-lg text-white font-bold">Contact Details</h3>
               <div class="address">
                 <div v-for="item in tm('about.contactDetails')">{{ item }}</div>
                 <a :href="`mailto:${email}`">{{ email }}</a>
               </div>
             </div>
             <div>
-              <div class="inline-flex items-center bg-zinc-800 px-5 py-3 text-[16px] font-bold cursor-pointer hover:bg-white hover:text-black transition-all">
+              <div class="inline-flex items-center px-5 py-3 bg-[#444] text-white font-bold cursor-pointer hover:bg-white hover:text-black transition-all">
                 <span class="i-mdi-download mr-2"></span>
                 <span>Download Resume</span>
               </div>
