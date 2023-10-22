57e5baf666ee1892c210b937fde136e0975a3d50
Alexandr Kurkin <2012stalker@gmail.com>
2023-10-22 16:00:10 +0300

    testing tresjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,8 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@nuxt/image',
     '@vueuse/nuxt',
-    'nuxt-simple-sitemap'
+    'nuxt-simple-sitemap',
+    '@tresjs/nuxt'
   ],
   // colorMode: {
   //   classSuffix: ''
