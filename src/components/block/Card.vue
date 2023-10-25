8b2dd33c322a829ce7338e17cb4f19b230aa4b23
Alexandr Kurkin <2012stalker@gmail.com>
2023-10-25 22:17:09 +0300

    added card overlay

--- src/components/block/Card.vue
+++ src/components/block/Card.vue
@@ -4,14 +4,45 @@
       <a
         :href="imgSrc"
         :data-sub-html="subHtml"
+        class="relative cursor-zoom-in"
       >
         <NuxtPicture
-          class="card__img cursor-zoom-in"
+          class="card__img"
           format="avif,webp"
           :src="nuxtPictureSrc"
           :alt="title"
           sizes="100vw sm:350px"
         />
+        <div
+          class="
+          card__overlay
+          absolute
+          top-0
+          left-0
+          opacity-0
+          w-full
+          flex
+          justify-center
+          items-center
+          h-full
+          bg-black/50
+          hover:opacity-100"
+        >
+          <div
+            class="
+            px-2.5
+            py-[5px]
+            rounded-3xl
+            text-white
+            text-[15px]
+            hover:text-accent
+            bg-[#2b2b2b]/70
+            cursor-pointer
+            z-10"
+          >
+            Show more
+          </div>
+        </div>
       </a>
     </lightgallery>
     <div class="card_title text-center">{{ title }}</div>
@@ -50,3 +81,11 @@
     zoom: false
   }
 </script>
+
+<style lang="scss" scoped>
+  .card {
+    &__overlay {
+      transition: 0.3s ease-in-out;
+    }
+  }
+</style>
