4a0032db83113cfe8ad97aa571eb58f9ec3d12da
Alexandr Kurkin <2012stalker@gmail.com>
2023-10-31 17:29:24 +0300

    css

--- src/components/section/About.vue
+++ src/components/section/About.vue
@@ -18,7 +18,7 @@
                 <a :href="`mailto:${email}`">{{ email }}</a>
               </div>
             </div>
-            <div class="btns-wrapper flex flex-col items-end gap-y-6">
+            <div class="btns-wrapper flex flex-col lg:items-end gap-y-6">
               <div>
                 <a
                   :href="t('about.btn2Link')"
