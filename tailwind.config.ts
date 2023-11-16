7af696ce57ead132729bb52b640a87b4351484b7
Alexandr Kurkin <2012stalker@gmail.com>
2023-11-16 15:28:34 +0300

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import { Config } from 'tailwindcss'
+import { type Config } from 'tailwindcss'
 import { getIconCollections, iconsPlugin } from '@egoist/tailwindcss-icons'
 
 export default <Config>{
